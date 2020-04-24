a3331bee93912ce2fc6d879a0f9897e571725557
tranzmatt <linux.matthew@gmail.com>
2020-04-24 11:02:44 -0400

    Fixes to layout, remove unwanted code

--- app.py
+++ app.py
@@ -25,77 +25,16 @@ def redirect_url():
            request.referrer or \
            url_for('index')
 
-@app.route("/list")
-def lists ():
-    #Display the all Tasks
-    devices_l = devices.find()
-    a1="active"
-    return render_template('index.html',a1=a1,devices=devices_l,t=title,h=heading)
-
 @app.route("/")
-@app.route("/uncompleted")
 def tasks ():
     #Display the Uncompleted Tasks
     devices_l = devices.find({"done":"no"})
     a2="active"
     return render_template('index.html',a2=a2,devices=devices_l,t=title,h=heading)
 
-
-@app.route("/completed")
-def completed ():
-    #Display the Completed Tasks
-    devices_l = devices.find({"done":"yes"})
-    a3="active"
-    return render_template('index.html',a3=a3,devices=devices_l,t=title,h=heading)
-
-@app.route("/done")
-def done ():
-    #Done-or-not ICON
-    id=request.values.get("_id")
-    task=devices.find({"_id":ObjectId(id)})
-    if(task[0]["done"]=="yes"):
-        devices.update({"_id":ObjectId(id)}, {"$set": {"done":"no"}})
-    else:
-        devices.update({"_id":ObjectId(id)}, {"$set": {"done":"yes"}})
-    redir=redirect_url()    
-
-    return redirect(redir)
-
-@app.route("/action", methods=['POST'])
-def action ():
-    #Adding a Task
-    name=request.values.get("name")
-    desc=request.values.get("desc")
-    date=request.values.get("date")
-    pr=request.values.get("pr")
-    devices.insert({ "name":name, "desc":desc, "date":date, "pr":pr, "done":"no"})
-    return redirect("/list")
-
-@app.route("/remove")
-def remove ():
-    #Deleting a Task with various references
-    key=request.values.get("_id")
-    devices.remove({"_id":ObjectId(key)})
-    return redirect("/")
-
-@app.route("/update")
-def update ():
-    id=request.values.get("_id")
-    task=devices.find({"_id":ObjectId(id)})
-    return render_template('update.html',tasks=task,h=heading,t=title)
-
-@app.route("/action3", methods=['POST'])
-def action3 ():
-    #Updating a Task with various references
-    name=request.values.get("name")
-    desc=request.values.get("desc")
-    date=request.values.get("date")
-    pr=request.values.get("pr")
-    id=request.values.get("_id")
-    devices.update({"_id":ObjectId(id)}, {'$set':{ "name":name, "desc":desc, "date":date, "pr":pr }})
-    return redirect("/")
-
 def date_to_epoch(the_datetime_t):
+    if not the_datetime_t:
+        return(None)
     try:
         the_datetime_out = datetime(*[int(v) for v in the_datetime_t.replace('T', '-').replace(':', '-').split('-')])
         the_naive = datetime.strptime (str(the_datetime_out), time_format)
@@ -115,40 +54,13 @@ def date_to_epoch(the_datetime_t):
 def search():
     #Searching a Task with various references
     
-    print('Running search')
-    
-    d = dict((db, [collection for collection in client[db].list_collection_names()]) for db in client.list_database_names())
-    print(json.dumps(d))
-    
     the_mac=request.values.get("macaddr")
     afterepoch = 0
     beforeepoch = time.time()
-    aftertime_t = None
-    beforetime_t = None
-    
-    afterdatetime_t = request.values.get("afterdatetime")
-    beforedatetime_t = request.values.get("beforedatetime")
 
-    print("_T AFTER: ",afterdatetime_t,": BEFORE : ",beforedatetime_t)
-    
-    # 2020-04-23T19:30
-    
-    beforeepoch_t = date_to_epoch(beforedatetime_t)
-    afterepoch_t = date_to_epoch(afterdatetime_t)
+    afterepoch_t = date_to_epoch(request.values.get("beforedatetime"))    
+    beforeepoch_t = date_to_epoch(request.values.get("afterdatetime"))
 
-
-    '''
-    afterdate_t=request.values.get("afterdate")
-    aftertime_t=request.values.get("aftertime")
-
-    beforedate_t=request.values.get("beforedate")
-    beforetime_t=request.values.get("beforetime")
-
-    print("AFTER: ",afterdate_t,":: ",aftertime_t," BEFORE: ",beforedate_t,":: ",beforetime_t)
-
-    #afterepoch_utc = local_datetime.astimezone(afterepoch_t.utc)   
-    print(afterepoch_out,"-->",beforeepoch_out)
-    '''
     try:
         afterepoch=int(afterepoch_t)
     except Exception as e:
@@ -163,8 +75,6 @@ def search():
         
     if (the_mac):
         mac_query.update ( { "Data.kismet_device_base_macaddr" : { "$regex": str(the_mac) }  }  ) 
-
-    print(mac_query)
 
     devices_l = devices.find(mac_query)
 
